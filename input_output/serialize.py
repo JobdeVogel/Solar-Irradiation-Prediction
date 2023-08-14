ae5a9433df94952e0fb92c3c7b611ab7e0c7993a
Job de Vogel <jobdvogel@gmail.com>
2023-08-14 14:39:34 +0200

    Added comments

--- input_output/serialize.py
+++ input_output/serialize.py
@@ -3,27 +3,51 @@ import System
 import json
 
 def polyline_to_points(polyline):
+    # Transform a polyline to points as tuples
     return [(point.X, point.Y, point.Z) for point in polyline]
 
 def points_to_polyline(points):
+    # Transform points as tuples to polylines
     points = System.Array[rg.Point3d](points)
     return rg.Polyline(points)
 
 def serialize(data):
-    # data = [[rg.Polyline()], [...]]
-        
+    """Serialize polyline data assuming the structure [[rg.Polyline], [...]]
+    Serializing can be used to make sure data is not changed in-place
+
+    Args:
+        data (list[list[rg.Polyline]]): polylines to serialize
+
+    Returns:
+        json string (str): serialized polylines in JSON format 
+    """
+    
+    # Iterate over all polylines in the data
     for i, outlines in enumerate(data):
         for j, polyline in enumerate(outlines):
+            # Transform the polyline data too points
             data[i][j] = polyline_to_points(polyline)
     
     return json.dumps(data)
 
 def deserialize(data):
+    """Deserialize polyline data assuming the structure [[rg.Polyline], [...]]
+
+    Args:
+        data (JSON str): JSON string containing polyline data
+
+    Returns:
+        data (list[list[rg.Polyline]]): polylines to serializee
+    """
+    
+    # Load the JSON data
     data = json.loads(data)
     for i, outlines in enumerate(data):
         for j, polyline in enumerate(outlines):
+            # Extract the points for the outline
             points = [rg.Point3d(point[0],point[1],point[2]) for point in polyline]
+            
+            # Transform the points to an outline
             data[i][j] = points_to_polyline(points)
 
     return data
-    
--- load_3dbag/outlines.py
+++ load_3dbag/outlines.py
@@ -1,12 +1,23 @@
+"""
+This module contains all functions regarding flat polylines, curves and rectangles
+"""
+
 from parameters.params import LOGGER
 import Rhino.Geometry as rg
 import System
-import sys
 
 from parameters.params import TRANSLATE_TO_ORIGIN, FSI, _SPLIT_TOLERANCE, MIN_AREA
 
 # Get the x,y domain of a bbox
 def get_domain(bbox):
+    """Get the x and y domain from a boundingbox
+
+    Args:
+        bbox (rg.BoundingBox): boundingbox
+
+    Returns:
+        domains: two tuples containing minima and maxima for x and y
+    """
     x = []
     y = []
     
@@ -23,47 +34,64 @@ def get_domain(bbox):
 
 # Divide main domain into sub domains
 def divide_domain(domain, size, min_coverage):
+    """Divide a domain in subdomains
+
+    Args:
+        domain (tuple): domain in shape min, max
+        size (float): size of the subdomains height and width
+        min_coverage (float): minimum overlap between subdomains
+
+    Returns:
+        sub_domains (list[tuple]): divided domains
+    """
+    # Extract the minimum and maximum from the original domain
     min_x, max_x = domain[0]
     min_y, max_y = domain[1]
     
+    # Compute the distance between minima and maxima for the domain
     x_size = abs(min_x) + abs(max_x)
     y_size = abs(min_y) + abs(max_y)
     
+    # Compute the step size
     step_size = size * min_coverage / 100
+    
+    # Compute number of steps in x and y direction
     x_steps = (x_size // step_size) - 1
     y_steps = (y_size // step_size) - 1
     
+    # Store the new subdivide domains
     sub_domains = []
     for i in range(int(x_steps)):
         for j in range(int(y_steps)):
+            # Compute a new domain
             x_domain = (min_x + i * step_size, min_x + i * step_size + size)
             y_domain = (min_y + j * step_size, min_y + j * step_size + size)
             
+            # Append the new domain to the returned domains
             sub_domains.append((x_domain, y_domain))
     
     return sub_domains
 
-# Generate bbox from domain and size
-def create_bbox(domain, bottom_treshold):
-    x_domain, y_domain = domain
-    x_center = (x_domain[0] + x_domain[1]) / 2
-    y_center = (y_domain[0] + y_domain[1]) / 2
-    
-    size = abs(x_domain[1] - x_domain[0])
-    
-    center = rg.Point3d(x_center, y_center, 0)
-    plane = rg.Plane(center, rg.Vector3d(0, 0, 1))
-    
-    interv = rg.Interval(-0.5 * size, 0.5 * size)
-    z_interv = rg.Interval(-0.1, size + 0.1)
-
-    box = rg.Box(plane, interv, interv, z_interv)
-    return box
-
 def generate_outlines_from_bbox(bbox, size, min_coverage):
+    """Generate a set of patch outlines that are used to indicate which meshes
+    should be extracted per sample
+
+    Args:
+        bbox (rg.BoundingBox): boundingbox of the entire mesh
+        size (float): width and height of the patches
+        min_coverage (float): factor indicating how much area may be repeated in sequential patches
+
+    Returns:
+        outlines (list[rg.Rectangle3d]): oulines of bbox as rectangles 
+    """
+    
+    #Get the domain of a boundingbox
     main_domain = get_domain(bbox)
+    
+    #Divide the main domain to subdomains
     sub_domains = divide_domain(main_domain, size, min_coverage)
     
+    #Generate a rectangle based on the subdomains
     outlines = []
     for domain in sub_domains:
         corner_0 = rg.Point3d(domain[0][0], domain[1][0], 0)
@@ -72,22 +100,32 @@ def generate_outlines_from_bbox(bbox, size, min_coverage):
 
     return outlines
 
-
-
-
-
-
 def includes_ground_outline(ground_outline, building_outline):
+    """Check if a building outline is inside a ground outline rectangle
+
+    Args:
+        ground_outline (rg.Rectange3d): Rectangle outline for the ground patch
+        building_outline (rg.Polyline): Polyline outline for a building
+
+    Returns:
+        int: -1 if outside, 1 if inside, 0 i intersecting, else None
+    """
+    # Boolean indicating if points are outside and inside
     outside = False
     inside = False
     
+    # Base relation is unset
     relation = rg.PointContainment.Unset
     
+    # Transform the ground outline polyline to Nurbscurve
     curve = ground_outline.ToNurbsCurve()
     
+    # For each point in the polyline
     for point in building_outline:
+        # Compute the relation with the ground outline
         relation = curve.Contains(point)
         
+        # Set the relations
         if relation == rg.PointContainment.Inside:
             inside = True
         elif relation == rg.PointContainment.Outside:
@@ -96,6 +134,7 @@ def includes_ground_outline(ground_outline, building_outline):
         if outside and inside:
             return 0
     
+    # If not intersecting, check the new relation
     if outside and not inside:
         return -1
     elif inside and not outside:
@@ -104,62 +143,101 @@ def includes_ground_outline(ground_outline, building_outline):
         return None
 
 def extract_building_outlines(wall_meshes, roof_meshes, tolerance=_SPLIT_TOLERANCE):
+    """Extract polyline building outlines from wall and roof meshes
+
+    Args:
+        wall_meshes (list[rg.Mesh]): list of wall meshes
+        roof_meshes (list[rg.Mesh]): list of roof meshes
+        tolerance (float, optional): Tolerance for adding naked edges to the outlines list. Defaults to _SPLIT_TOLERANCE.
+
+    Returns:
+        building_outlines (list[list[rg.Polyline]]): building outlines
+        building_heights (list[float]): heights individual buildings
+    """
+    
+    # Store the building outlines and heihgts
     building_outlines = []
     building_heights = []
     
+    # Iterate over both walls and roofs
     for i, (wall, roof) in enumerate(zip(wall_meshes, roof_meshes)):
+        # Store temporary outlines for a specific building
         outlines = []
         
+        # Get the naked edges from the wall
         lines = wall.GetNakedEdges()
         
-        for naked in lines:
-            height = naked.CenterPoint().Z
+        # If the extractionn succeeded
+        if lines != None:       
+            # Iterate over the naked lines
+            for naked in lines:
+                # Check the height of the naked line
+                height = naked.CenterPoint().Z
+                
+                # If the height is lower than the tolerance, add the naked line to the outlines
+                if height < tolerance:
+                    outlines.append(naked)
             
-            if height < tolerance:
-                outlines.append(naked)
-        
-        for outline in outlines:
-            outline.ReduceSegments(tolerance)
-        
-        if len(outlines) != 0:
-            building_outlines.append(outlines)
-            building_heights.append(roof.Faces.GetFaceCenter(0).Z)
+            # Reduce the number of segments in the outlines
+            for outline in outlines:
+                outline.ReduceSegments(tolerance)
+            
+            # Check if the building is not floating above the ground
+            if len(outlines) != 0:
+                # If not, append the outlines and the building height
+                building_outlines.append(outlines)
+                building_heights.append(roof.Faces.GetFaceCenter(0).Z)
+            else:
+                LOGGER.warning('Mesh ' + str(i) + ' does not have naked edges with height lower than tolerance ' + str(tolerance) + '. This building if floating above the ground.')
         else:
-            LOGGER.warning('Mesh ' + str(i) + ' does not have outline with height lower than tolerance ' + str(tolerance))
+            LOGGER.warning('Mesh ' + str(i) + ' naked edges extraction failed. This mesh is most likely closed.')
     
     return building_outlines, building_heights
 
 # Find which polyline is the outer polyline of a mesh surface
 def find_outer_polyline(polylines):
-    # IMPROVE:
-    # Rough assumption that outer polyline is always longest length
+    """Give multiple polylines, find which polyline has the longest length, and is most likely  the outer polyline
+    
+    # ! IMPROVE: This assumption is theoratically not always correct
+
+    Args:
+        polylines (list[rg.Polyline]): list of polylines
+
+    Returns:
+        outer + inner (list[rg.Polyline]): ordered list of polylines
+    """
+    # Store the lengths of the polylines and which polylines are inner polylines
     lengths = []
     inner = []
     
+    # Forr each polyline, compute the length
     for polyline in polylines:
         lengths.append(polyline.Length)
         inner.append(polyline)
     
+    # Delete the polyline with the longest length
     idx = lengths.index(max(lengths))
     outer = [polylines[idx]]
     del inner[idx]
     
     return outer + inner
 
-def find_segments(segments, segmented_curve, base_curve):
+def find_segments(segments, base_curve):
     """Find which segments from a segmented curve should be kept and which
     should be deleted.
     Inputs:
         segments: The segments as polylines
-        segmented_curve: The The curve that was segmented
         base_curve: The curve used for containment
     Output:
         polylines: the segments that should be kept"""
-        
+    
+    #Store the polylines that should be kept    
     polylines = []
     for segment in segments:
+        #Transform the segment from rg.Polyline to rg.NurbsCurve
         segment = segment.ToNurbsCurve()
         
+        #Get 
         domain = segment.Domain
         midpoint_parameter = domain.Mid
         
@@ -202,11 +280,11 @@ def cut_polyline(ground_outline, building_outline, tolerance=_SPLIT_TOLERANCE, m
     building_segments = building_curve.Split(parameters_building)
     
     # Get the inner building segment
-    building_segments = find_segments(building_segments, building_curve, ground_curve)
+    building_segments = find_segments(building_segments, ground_curve)
 
     # Get the ground segment
     ground_polylines = [segment.TryGetPolyline()[1] for segment in ground_segments]
-    ground_segments = find_segments(ground_polylines, ground_curve, building_curve)
+    ground_segments = find_segments(ground_polylines, building_curve)
 
     new_building_curves = System.Array[rg.NurbsCurve](building_segments + ground_segments)
     joined_building_curve = rg.Curve.JoinCurves(new_building_curves, tolerance)
@@ -217,7 +295,6 @@ def cut_polyline(ground_outline, building_outline, tolerance=_SPLIT_TOLERANCE, m
     if len(polylines) == 0:
         LOGGER.warning("cut_polyline() was not able to extract polylines")
     
-    # ! NOT IMPLEMENTED YET
     valid_polylines = []
     for polyline in polylines:
         template = polyline.Duplicate()
@@ -266,14 +343,34 @@ def compute_FSI(ground_outline, building_outlines):
     
     return sum(building_areas) / ground_area, ground_area, building_areas
     
-def generate_building_outlines(ground_outline, all_building_outlines, heights, min_area, translate_to_origin=TRANSLATE_TO_ORIGIN, fsi=FSI):    
-    # Compute the polylines inside the ground_outline
+def generate_building_outlines(ground_outline, all_building_outlines, heights, translate_to_origin=TRANSLATE_TO_ORIGIN, fsi=FSI):    
+    """Compute the building outlines that are inside a ground outline. If a building polyline intersects
+    with the ground outline, the building outlines are splitted in multiple segmenets and then closed.
+
+    Args:
+        ground_outline (rg.Rectangle3d): ground rectangle outline
+        all_building_outlines (list[list[rg,Polyline]]): all building outlines
+        heights (listt[float]): all heights of the buildings
+        translate_to_origin (bool, optional): indicates if outlines should be moved to origin. Defaults to TRANSLATE_TO_ORIGIN.
+        fsi (bool, optional): Indicates if fsi should be computed. Defaults to FSI.
+
+    Returns:
+        building_outlines (list[list[rg.Polyline]]): outlines for the buildings (including splitted intersecting building outlines)
+        courtyard_outlines (list[list[rg.Polyline]]): outlines for the courtyards, empty if no courtyard available
+        building_heights (list[float]): heights of the included buildings
+        FSI_score (float): fsi score, None if not computed
+        envelope_area (float): area of envelope in m2, None if not computed 
+        building_area (float): area of all building, courtyards included in m2, None if not computed
+    """
+    
+    # Store the polylines and heights
     included_polylines = []
     courtyards = []
     building_heights = []
     num_of_courtyards = 0
     
-    for i, (polylines, height) in enumerate(zip(all_building_outlines, heights)):
+    # Iterate over all outlines and the corresponding heights
+    for polylines in zip(all_building_outlines, heights):
         # The outer polyline of the building
         outer = polylines[0]
         
@@ -282,7 +379,7 @@ def generate_building_outlines(ground_outline, all_building_outlines, heights, m
             # Check if outer polyline inside the ground_outline
             relation = includes_ground_outline(ground_outline, outer)
             
-            # Inside
+            # This building is inside the ground outline
             if relation == 1:
                 included_polylines.append([outer])
                 building_heights.append(height)
@@ -290,12 +387,11 @@ def generate_building_outlines(ground_outline, all_building_outlines, heights, m
                 # No courtyards are available, so add empty list
                 courtyards.append([])
             
-            # Intersectin
+            # This building is intersecting with the ground outline
             elif relation == 0:
                 # Cut the polyline and add it to included_polylines
                 polylines = cut_polyline(ground_outline, outer)
                                
-                # ! Check if this works!!!
                 if len(polylines) > 0:                   
                     # if smallest_area > min_area:
                     included_polylines.append(polylines)
@@ -317,10 +413,7 @@ def generate_building_outlines(ground_outline, all_building_outlines, heights, m
                 if relation == 1:
                     temp_courtyard_polylines.append(inner)
                     num_of_courtyards += 1
-                # elif relation == 0:
-                #     polylines = cut_polyline(ground_outline, inner)
-                #     temp_courtyard_polylines += polylines
-            
+                    
             # Check if the outer polyline is inside the groundline
             relation = includes_ground_outline(ground_outline, outer)
             
@@ -356,8 +449,8 @@ def generate_building_outlines(ground_outline, all_building_outlines, heights, m
             for outline in outlines:
                 translate(outline, ground_outline)        
         
+        # Translate the ground outline to the origin    
         translate(ground_outline, ground_outline)
-    
     
     if fsi:
         FSI_score, envelope_area, building_area = compute_FSI(ground_outline, included_polylines)
@@ -371,9 +464,3 @@ def generate_building_outlines(ground_outline, all_building_outlines, heights, m
 
     LOGGER.debug(f'Generated {len(building_outlines)} building_outlines and {num_of_courtyards} courtyards with FSI {round(FSI_score, 2)}.')
     return building_outlines, courtyard_outlines, building_heights, FSI_score, envelope_area, building_area
-
-# ground_outline = ground_outlines[int(idx)]
-# building_outlines, courtyard_outlines, heights, FSI, envelope_area, building_area = main(ground_outlines, wall_meshes, heights, size, idx, FSI=FSI)
-
-# courtyard_outlines = th.list_to_tree(courtyard_outlines, source=[0])
-# building_outlines = th.list_to_tree(building_outlines, source=[0])
