80c6d50f6dc90cfe2f9024b9f5a999462a9c0e0b
Job de Vogel <jobdvogel@gmail.com>
2023-09-13 18:35:32 +0200

    Fixed augmentation bug

--- augment/augmentation.py
+++ augment/augmentation.py
@@ -2,42 +2,25 @@ import Rhino.Geometry as rg
 from math import pi
 import System
 
-def augment(mesh, pointcloud, number, 
-                rot_axis = rg.Vector3d(0,0,1), 
-                rot_point = rg.Point3d(0,0,0)
-                ):
-
-    pointcloud = System.Array[rg.Point3d](pointcloud)
-
-    meshes = [mesh.Duplicate() for _ in range(number)]
-    pointclouds = [rg.PointCloud(pointcloud) for _ in range(number)]
-
-    angles = [i * 0.5 * pi for i in range(number)]
-    
-    for mesh, pointcloud, angle in zip(meshes, pointclouds, angles):
-        mesh.Rotate(angle, rot_axis, rot_point)
-        pointcloud.Rotate(angle, rot_axis, rot_point)
-    
-    pointclouds = [pointcloud.GetPoints() for pointcloud in pointclouds]
-    return meshes, pointclouds
+import sys
 
 def augment(meshes, pointcloud, normalcloud, number, 
                 rot_axis = rg.Vector3d(0,0,1), 
                 rot_point = rg.Point3d(0,0,0),
                 detailed_meshes=[None, None, None]):
 
-    pointcloud = System.Array[rg.Point3d](pointcloud)
-    pointclouds = [rg.PointCloud(pointcloud) for _ in range(number)]
+    pointcloud = rg.PointCloud(System.Array[rg.Point3d](pointcloud))
+    pointclouds = []
     
-    normalcloud = System.Array[rg.Point3d]([rg.Point3d(normal.X, normal.Y, normal.Z) for normal in normalcloud])
-    normalclouds = [rg.PointCloud(normalcloud) for _ in range(number)]
+    normalcloud = rg.PointCloud(System.Array[rg.Point3d]([rg.Point3d(normal.X, normal.Y, normal.Z) for normal in normalcloud]))
+    normalclouds = []
     
     angles = [i * 0.5 * pi for i in range(number)]
     
     augmented_meshes = []
     augmented_detailed_meshes = []
 
-    for angle, pointcloud, normalcloud in zip(angles, pointclouds, normalclouds):
+    for angle in angles:
         temp_meshes = []
         temp_detailed_meshes = []
         
@@ -53,10 +36,17 @@ def augment(meshes, pointcloud, normalcloud, number,
                 
                 temp_detailed_meshes.append(augmented_mesh)
 
-        pointcloud.Rotate(angle, rot_axis, rot_point)
-        normalcloud.Rotate(angle, rot_axis, rot_point)
         augmented_meshes.append(temp_meshes)
         augmented_detailed_meshes.append(temp_detailed_meshes)
+
+        temp_pointcloud = pointcloud.Duplicate()
+        temp_normalcloud = normalcloud.Duplicate()
+
+        temp_pointcloud.Rotate(angle, rot_axis, rot_point)
+        temp_normalcloud.Rotate(angle, rot_axis, rot_point)
+        
+        pointclouds.append(temp_pointcloud)
+        normalclouds.append(temp_normalcloud)
     
     pointclouds = [cloud.GetPoints() for cloud in pointclouds]
     normalclouds = [cloud.GetPoints() for cloud in normalclouds]
