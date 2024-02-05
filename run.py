9a033a2780b19dda362f88cd3ce6544ede65df03
Job de Vogel <jobdvogel@gmail.com>
2024-02-05 14:22:02 +0100

    Added exceptions for extreme cases

--- run.py
+++ run.py
@@ -11,42 +11,48 @@ import sys
 import gc
 
 def task(file, logger):
-    bag_file = os.path.join(BAG_PATH, file)
-       
-    geometry_path = os.path.join(GEOMETRY_PATH, file[:-4])
-    irradiance_path = os.path.join(IRRADIANCE_PATH, file[:-4])
-    raw_path = os.path.join(RAW_PATH, file[:-4])
-    
-    if not os.path.exists(geometry_path):
-        os.makedirs(geometry_path)
-    
-    if not os.path.exists(irradiance_path):
-        os.makedirs(irradiance_path)
-    
-    if not os.path.exists(raw_path):
-        os.makedirs(raw_path)
-    
-    main(bag_file, 0, logger, geometry_path=geometry_path, irradiance_path=irradiance_path, raw_path=raw_path)
+    try:
+        bag_file = os.path.join(BAG_PATH, file)
+
+        geometry_path = os.path.join(GEOMETRY_PATH, file[:-4])
+        irradiance_path = os.path.join(IRRADIANCE_PATH, file[:-4])
+        raw_path = os.path.join(RAW_PATH, file[:-4])
+
+        if not os.path.exists(geometry_path):
+            os.makedirs(geometry_path)
+
+        if not os.path.exists(irradiance_path):
+            os.makedirs(irradiance_path)
+
+        if not os.path.exists(raw_path):
+            os.makedirs(raw_path)
+
+        main(bag_file, 0, logger, geometry_path=geometry_path, irradiance_path=irradiance_path, raw_path=raw_path)
+    except:
+        logger.critical("Task in multiprocessing failed!")
 
 def process(info):
-    idx, file = info
+    try:
+        idx, file = info
 
-    # Initialize a logger
-    identifier = file.split("/")[-1][:-4]
-    
-    print(f"(stdout:) Initialized process {idx} for file {file}")
-    
-    if idx == 0:
-        logger = generate_logger(identifier=identifier, stdout=True)
-        logger.info(f"Sending log output to stdout for process 0, file: {file}")
-    else:
-        logger = generate_logger(identifier=identifier, stdout=False)
-    
-    task(file, logger)
-    
-    del identifier
-    del logger
-    gc.collect()   
+        # Initialize a logger
+        identifier = file.split("/")[-1][:-4]
+
+        print(f"(stdout:) Initialized process {idx} for file {file}")
+
+        if idx == 0:
+            logger = generate_logger(identifier=identifier, stdout=True)
+            logger.info(f"Sending log output to stdout for process 0, file: {file}")
+        else:
+            logger = generate_logger(identifier=identifier, stdout=False)
+
+        task(file, logger)
+
+        del identifier
+        del logger
+        gc.collect()   
+    except:
+        logger.critical("Process in multiprocessing failed!")
 
 if __name__ =='__main__':
     MAIN_LOGGER = generate_logger('main', stdout=True)  
